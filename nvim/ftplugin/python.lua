cc030e41f4792165e715fe6e171c241340108147
Dachi Chang <dachi.c@migo.io>
2023-02-01 23:25:55 +0800

    use autopep8 instead pyright lsp formmater

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -4,4 +4,4 @@ local keymap = vim.keymap.set
 g.autopep8_max_line_length = 140
 g.autopep8_disable_show_diff = 1
 
-keymap("n", "<F8>", ":Autopep8<CR>")
+keymap("n", "<F12>", ":Autopep8<CR>")
--- nvim/lua/plugin_config/lsp.lua
+++ nvim/lua/plugin_config/lsp.lua
@@ -34,7 +34,7 @@ keymap.set('n', 'en', diagnostic.goto_next, opts)
 -- for each lsp-server setup
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   keymap.set('n', 'gt', buf.hover, bufopts)
   keymap.set('n', 'gd', buf.definition, bufopts)
@@ -43,7 +43,15 @@ local on_attach = function(_, bufnr)
   keymap.set('n', 'gT', buf.type_definition, bufopts)
   keymap.set('n', 'gr', buf.references, bufopts)
   keymap.set('n', '<F2>', buf.rename, bufopts)
-  keymap.set('n', '<F12>', function() buf.format { async = true } end, bufopts)
+
+  local without_formatter = {
+    'pyright', -- use vim-autopep8 instead, because pyright not support buffer formatter
+  }
+  for _, c in ipairs(without_formatter) do
+    if c ~= client.name then
+      keymap.set('n', '<F12>', function() buf.format { async = true } end, bufopts)
+    end
+  end
 end
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
