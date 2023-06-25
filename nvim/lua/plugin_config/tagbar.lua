f595447e09c386a381df52d266b0e4999025af98
Dachi Chang <dachi.chang@idtechproducts.com>
2023-06-25 21:41:01 +0800

    jump to tagbar

--- nvim/lua/plugin_config/tagbar.lua
+++ nvim/lua/plugin_config/tagbar.lua
@@ -2,4 +2,5 @@ local keymap = vim.keymap.set
 local opt = vim.opt
 
 keymap("n", "<leader>8", ":TagbarToggle<CR>")
+keymap("n", "g8", ":TagbarOpen fj<CR>") -- always jump to the Tagbar window, opening it first if necessary
 opt.updatetime = 250
