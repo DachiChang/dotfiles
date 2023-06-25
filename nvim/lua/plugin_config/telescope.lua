8f7e7ebb7e7b946e493a3c14f806c0bfd2641b98
Dachi Chang <dachi.chang@idtechproducts.com>
2023-06-25 15:43:38 +0800

    telescope-tab filier nvim-tree, tagbar

--- nvim/lua/plugin_config/telescope.lua
+++ nvim/lua/plugin_config/telescope.lua
@@ -28,6 +28,29 @@ keymap('n', '<leader>k', builtin.keymaps, {})
 -- search and jump to tabs
 require('telescope-tabs').setup {
   show_preview = false,
+  entry_formatter = function(tab_id, buffer_ids, file_names, file_paths, is_current)
+    local exclude_file_names = {
+      "NvimTree",
+      "Tagbar",
+    }
+
+    local is_exclude = function (filename)
+      for _, exclude_file_name in ipairs(exclude_file_names) do
+        if filename:find(exclude_file_name) then
+          return true
+        end
+      end
+      return false
+    end
+
+    local filter_names = {}
+    for _, file_name in ipairs(file_names) do
+      if not is_exclude(file_name) then
+        table.insert(filter_names, file_name)
+      end
+    end
+    return string.format('%d: %s%s', tab_id, table.concat(filter_names, ', '), is_current and ' <' or '')
+  end,
 }
 local tabs = require('telescope-tabs')
 keymap('n', '<leader>t', tabs.list_tabs, {})
