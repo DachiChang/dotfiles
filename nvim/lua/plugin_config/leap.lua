9b181352090b75e4089f19624b2e3b014c5bcc8e
Dachi Chang <dachi.chang@idtechproducts.com>
2023-06-25 19:52:35 +0800

    not to use default leap keymap

--- nvim/lua/plugin_config/leap.lua
+++ nvim/lua/plugin_config/leap.lua
@@ -1,20 +1,17 @@
-require('leap').add_default_mappings()
 require('leap').opts.safe_labels = {}
-
 -- enable global search
 local keymap = vim.keymap
 keymap.set({ 'n', 'v' }, '<leader>\\', function()
-  local focusable_windows_on_tabpage = vim.tbl_filter(
-    function(win) return vim.api.nvim_win_get_config(win).focusable end,
-    vim.api.nvim_tabpage_list_wins(0)
-  )
-  require('leap').leap { target_windows = focusable_windows_on_tabpage }
+  local current_window = vim.fn.win_getid()
+  require('leap').leap { target_windows = { current_window } }
 end)
-keymap.del('v', 'x')
-keymap.del('v', 'X')
-keymap.del('n', 's')
-keymap.del('n', 'S')
-keymap.del({ 'n', 'v' }, 'gs')
+-- keymap.set({ 'n', 'v' }, '<leader>\\', function()
+--   local focusable_windows_on_tabpage = vim.tbl_filter(
+--     function(win) return vim.api.nvim_win_get_config(win).focusable end,
+--     vim.api.nvim_tabpage_list_wins(0)
+--   )
+--   require('leap').leap { target_windows = focusable_windows_on_tabpage }
+-- end)
 
 -- highlight group
 local api = vim.api
