5bf6b290f51d339ee0611f110da92037c46e9724
Dachi Chang <dachi.chang@idtechproducts.com>
2023-07-08 14:23:15 +0800

    only in the context

--- nvim/lua/plugin_config/leap.lua
+++ nvim/lua/plugin_config/leap.lua
@@ -1,18 +1,18 @@
 require('leap').opts.safe_labels = {}
 -- enable global search
 local keymap = vim.keymap
--- keymap.set({ 'n', 'v' }, '<leader> ', function()
---   local current_window = vim.fn.win_getid()
---   require('leap').leap { target_windows = { current_window } }
--- end)
-
-keymap.set({ 'n', 'v' }, '<leader>\\', function()
-  local focusable_windows_on_tabpage = vim.tbl_filter(
-    function(win) return vim.api.nvim_win_get_config(win).focusable end,
-    vim.api.nvim_tabpage_list_wins(0)
-  )
-  require('leap').leap { target_windows = focusable_windows_on_tabpage }
+keymap.set({ 'n', 'v' }, '?', function()
+  local current_window = vim.fn.win_getid()
+  require('leap').leap { target_windows = { current_window } }
 end)
+
+-- keymap.set({ 'n', 'v' }, '?', function()
+--   local focusable_windows_on_tabpage = vim.tbl_filter(
+--     function(win) return vim.api.nvim_win_get_config(win).focusable end,
+--     vim.api.nvim_tabpage_list_wins(0)
+--   )
+--   require('leap').leap { target_windows = focusable_windows_on_tabpage }
+-- end)
 
 -- highlight group
 local api = vim.api
