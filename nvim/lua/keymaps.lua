d2f9cd3ecbcf40ee336639fd41c48060b6ad0fec
Dachi Chang <dachi.chang@idtechproducts.com>
2023-07-14 09:55:16 +0800

    use '-' to switch window

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -27,7 +27,7 @@ keymap("n", "<F5>", ":e!<CR>", default_opts)
 
 -- Panes
 keymap("n", "=", "<C-w>=", default_opts)
-keymap("n", "s", "<C-w>x", default_opts)
+keymap("n", "-", "<C-w>x", default_opts)
 keymap("n", "<C-h>", "<C-w>h", default_opts)
 keymap("n", "<C-j>", "<C-w>j", default_opts)
 keymap("n", "<C-k>", "<C-w>k", default_opts)
