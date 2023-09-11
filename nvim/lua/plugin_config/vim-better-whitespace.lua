0028a856e75fccd9064b53ca02b293e062658e81
Dachi Chang <dachi.chang@idtechproducts.com>
2023-09-11 11:52:17 +0800

    fix toggleterm problem

--- nvim/lua/plugin_config/vim-better-whitespace.lua
+++ nvim/lua/plugin_config/vim-better-whitespace.lua
@@ -1,4 +1,8 @@
+local g = vim.g
 local keymap = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
+g.better_whitespace_filetypes_blacklist = {
+  "toggleterm",
+}
 keymap("n", "tm", ":StripWhitespace<CR>", default_opts)
