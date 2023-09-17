38fca0461aecfb4a4d85a02a2803c809a79149ba
Dachi Chang <dachi.chang@idtechproducts.com>
2023-09-17 23:17:06 +0800

    don't wrap scan

--- nvim/lua/plugin_config/illuminate.lua
+++ nvim/lua/plugin_config/illuminate.lua
@@ -2,8 +2,12 @@ local lsp_keyword_mark = require('illuminate')
 local default_opts = { nowait = true, noremap = true, silent = true }
 
 local keymap = vim.keymap
-keymap.set('n', ']', lsp_keyword_mark.goto_next_reference, default_opts)
-keymap.set('n', '[', lsp_keyword_mark.goto_prev_reference, default_opts)
+keymap.set('n', ']', function ()
+  lsp_keyword_mark.goto_next_reference(false)
+end , default_opts)
+keymap.set('n', '[', function ()
+  lsp_keyword_mark.goto_prev_reference(false)
+end, default_opts)
 
 local api = vim.api
 api.nvim_set_hl(0, "IlluminatedWordText", { underline = true })
