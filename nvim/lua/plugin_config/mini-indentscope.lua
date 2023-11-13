d37c1229206fecbbce175b496c20267677ddecc1
Dachi Chang <dachi.chang@idtechproducts.com>
2023-11-13 09:30:58 +0800

    fix vi conflict with viw

--- nvim/lua/plugin_config/mini-indentscope.lua
+++ nvim/lua/plugin_config/mini-indentscope.lua
@@ -19,7 +19,7 @@ indentscope.setup {
   -- Module mappings. Use `''` (empty string) to disable one.
   mappings = {
     -- Textobjects
-    object_scope = 'i',
+    object_scope = 'ii',
     object_scope_with_border = 'ai',
 
     -- Motions (jump to respective border line; if not present - body line)
