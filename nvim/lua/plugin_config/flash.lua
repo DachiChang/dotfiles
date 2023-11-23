ef0a9438558d91c0d86086ef3e15233b19b55786
Dachi Chang <dachi.chang@idtechproducts.com>
2023-11-23 16:10:20 +0800

    use default config is enough

--- nvim/lua/plugin_config/flash.lua
+++ nvim/lua/plugin_config/flash.lua
@@ -1,15 +1,4 @@
-require('flash').setup {
-  modes = {
-    search = {
-      highlight = {
-        backdrop = true,
-      },
-    },
-    char = {
-      enabled = true,
-    },
-  },
-}
+require('flash').setup {}
 
 local api = vim.api
 api.nvim_set_hl(0, "FlashLabel", { ctermbg = 162 })
