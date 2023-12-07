3f66031fe88d69c64f46a6f894fca4fc840326ec
Dachi Chang <dachi.chang@idtechproducts.com>
2023-12-07 09:25:34 +0800

    don't need the keybinding because nvim built-in functional

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -33,8 +33,6 @@ keymap("n", "<S-Left>", ":vertical resize -3<CR>", default_opts)
 keymap("n", "<S-Down>", ":resize -3<CR>", default_opts)
 keymap("n", "<S-Up>", ":resize +3<CR>", default_opts)
 
--- Search for visually selected text
-keymap("v", "//", [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], default_opts)
 -- Temporary disable hightlight
 keymap("n", "<ESC>", ":noh<CR>", default_opts)
 -- Use cp shortcut to copy editing file path
