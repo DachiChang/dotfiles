a89e2f6415a80902acbe363b5ad302dd5944a5de
Dachi Chang <dachi.chang@idtechproducts.com>
2023-12-14 23:07:31 +0800

    use F10 to enable MarkdownPreviewToggle

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -2,12 +2,15 @@ return {
   "iamcco/markdown-preview.nvim",
   enabled = true,
   ft = 'markdown',
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
   config = function()
     local g = vim.g
     g.mkdp_theme = 'light'
+
+    local keymap = vim.keymap.set
+    local default_opts = { noremap = true, silent = true }
+    keymap("n", "<F10>", ":MarkdownPreviewToggle<CR>", default_opts)
   end,
 }
