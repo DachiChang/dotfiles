fbca6b9ebcd2acf4f55f3c7959f546a02e3b8f1f
Dachi Chang <dachi.chang@idtechproducts.com>
2023-12-17 23:08:18 +0800

    bind F4 for highlight by treesitter and enable scope selection

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -26,7 +26,16 @@ return {
       auto_install = true,
       highlight = {
         enable = false,
-      }
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '+',
+          node_incremental = '+',
+          scope_incremental = false,
+          node_decremental = '_',
+        },
+      },
     }
 
     -- user treesitter to provider fold functional
@@ -35,5 +44,10 @@ return {
     opt.foldexpr = "nvim_treesitter#foldexpr()"
     opt.foldenable = true
     opt.foldlevelstart = 99
+
+    -- enable highlight on F4
+    local keymap = vim.keymap.set
+    local default_opts = { noremap = true, silent = true }
+    keymap("n", "<F4>", ":TSBufToggle highlight<CR>", default_opts)
   end
 }
