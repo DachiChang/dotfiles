267b2915fb92c57bf339f3f9633478e99d299381
Dachi Chang <dachi.chang@idtechproducts.com>
2024-03-04 09:25:49 +0800

    refactor nvim tree keymap

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -12,15 +12,32 @@ return {
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
       end
 
-      api.config.mappings.default_on_attach(bufnr)
+      -- NOTE: disable default keymap setting
+      -- api.config.mappings.default_on_attach(bufnr)
 
       local keymap = vim.keymap.set
-      keymap("n", "O", api.tree.expand_all, opts("Expand All"))
-      keymap("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
-      keymap("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
-      keymap("n", "<C-t>", api.node.open.tab, opts("Open: New Tab or Switch to Tab"))
-      keymap("n", "t", api.node.open.tab, opts("Open: New Tab or Switch to Tab"))
+
+      keymap("n", ".", api.tree.change_root_to_node, opts("CD"))
+      keymap("n", "<CR>", api.node.open.edit, opts("Open"))
       keymap("n", "?", api.tree.toggle_help, opts("Help"))
+      keymap("n", "F", api.live_filter.clear, opts("Live Filter: Clear"))
+      keymap("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Filter: Dotfiles"))
+      keymap("n", "O", api.tree.expand_all, opts("Expand All"))
+      keymap("n", "R", api.tree.reload, opts("Refresh"))
+      keymap("n", "Y", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+      keymap("n", "a", api.fs.create, opts("Create File Or Directory"))
+      keymap("n", "c", api.fs.copy.node, opts("Copy"))
+      keymap("n", "d", api.fs.remove, opts("Delete"))
+      keymap("n", "f", api.live_filter.start, opts("Live Filter: Start"))
+      keymap("n", "i", api.node.show_info_popup, opts("Info"))
+      keymap("n", "p", api.fs.paste, opts("Paste"))
+      keymap("n", "q", api.tree.close, opts("Close"))
+      keymap("n", "r", api.fs.rename, opts("Rename"))
+      keymap("n", "s", api.node.open.horizontal, opts("Open: Horizontal Split"))
+      keymap("n", "t", api.node.open.tab, opts("Open: New Tab"))
+      keymap("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+      keymap("n", "x", api.fs.cut, opts("Cut"))
+      keymap("n", "y", api.fs.copy.filename, opts("Copy Name"))
     end
 
     local nvim_tree = require('nvim-tree')
