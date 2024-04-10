dd49e33ce61ad4305b588d67ab3fff0c748c4d7e
Dachi Chang <dachi.chang@idtechproducts.com>
2024-04-10 13:32:58 +0800

    remove vim-maximizer plugin

--- nvim/.config/nvim/lua/plugins/vim-maximizer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'szw/vim-maximizer',
-  enabled = true,
-  config = function()
-    local default_opts = { noremap = true, silent = true }
-    local keymap = vim.keymap.set
-    keymap("n", "+", ":MaximizerToggle<CR>", default_opts)
-  end
-}
