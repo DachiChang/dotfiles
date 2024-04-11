48f1abbf7fb08aa279a8df9ce05438ca487dbeaf
Dachi Chang <dachi.chang@idtechproducts.com>
2024-04-11 13:42:11 +0800

    fix bug https://github.com/Exafunction/codeium.vim/issues/351

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -1,14 +1,19 @@
 return {
   'Exafunction/codeium.vim',
   enabled = true,
-  tag = '1.8.10',
+  tag = '1.8.16',
+  init = function()
+    local g = vim.g
+
+    g.codeium_disable_bindings = 1
+    g.codeium_filetypes = {
+      TelescopePrompt = false,
+    }
+  end,
   config = function()
     local default_opts = { expr = true, noremap = true, silent = true }
     local keymap = vim.keymap.set
     local fn = vim.fn
-    local g = vim.g
-
-    g.codeium_disable_bindings = 1
 
     keymap("i", "<C-y>", function()
       return fn["codeium#Accept"]()
