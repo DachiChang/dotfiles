5dee3c689a10377eac0e867ad03fa2866420bfd0
Dachi Chang <dachi.chang@idtechproducts.com>
2024-05-29 23:46:54 +0800

    flash config

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -3,7 +3,16 @@ return {
   'folke/flash.nvim',
   enabled = true,
   config = function()
+    local keymap = vim.keymap.set
+    local default_opts = { noremap = true, silent = true }
     local flash = require('flash')
-    flash.setup()
+    flash.setup {
+      label = {
+        rainbow = {
+          enabled = true,
+        },
+      },
+    }
+    keymap({ "n", "x", "o" }, "s", flash.jump, default_opts)
   end,
 }
