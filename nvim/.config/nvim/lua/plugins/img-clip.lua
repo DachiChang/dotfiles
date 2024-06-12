f8dcd61804a6e2b41f18822b9e4aebcea1c34fb6
Dachi Chang <dachi.chang@idtechproducts.com>
2024-06-12 10:30:28 +0800

    add F3 to do command paste Image

--- nvim/.config/nvim/lua/plugins/img-clip.lua
+++ nvim/.config/nvim/lua/plugins/img-clip.lua
@@ -5,5 +5,9 @@ return {
   config = function()
     local img_clip = require('img-clip')
     img_clip.setup()
+
+    -- toggle Image Paste on F3
+    local keymap = vim.keymap.set
+    keymap("n", "<F3>", ":PasteImage<CR>")
   end
 }
