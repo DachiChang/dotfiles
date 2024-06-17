ea447e8869e22b0d6ac5cbecd03d301f71805d2d
Dachi Chang <dachi.chang@idtechproducts.com>
2024-06-17 17:50:29 +0800

    remove mini.comment setup, because nvim0.10 has merged the feature.

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -3,28 +3,6 @@ return {
   enabled = true,
   version = '*',
   config = function()
-    local mini_comment = require('mini.comment')
-    mini_comment.setup {
-      options = {
-        custom_commentstring = nil,
-        ignore_blank_line = false,
-        start_of_line = false,
-        pad_comment_parts = true,
-      },
-      mappings = {
-        comment = 'cc',
-        comment_line = 'cc',
-        comment_visual = 'cc',
-        textobject = 'cc',
-      },
-      hooks = {
-        -- Before successful commenting. Does nothing by default.
-        pre = function() end,
-        -- After successful commenting. Does nothing by default.
-        post = function() end,
-      },
-    }
-
     local mini_indentscope = require('mini.indentscope')
     mini_indentscope.setup {
       draw = {
