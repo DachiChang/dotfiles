fabe0f7688073b54e9348a94856518eb37077d2f
Dachi Chang <dachi.chang@idtechproducts.com>
2024-07-03 13:07:45 +0800

    use gn, gp to select next variable reference to avoid keymap conflict

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -1,16 +1,15 @@
 return {
   'RRethy/vim-illuminate',
   enabled = true,
-  event = "VeryLazy",
   config = function()
     local lsp_keyword_mark = require('illuminate')
-    local default_opts = { nowait = true, noremap = true, silent = true }
+    local default_opts = { noremap = true, silent = true }
 
     local keymap = vim.keymap.set
-    keymap("n", "]", function()
+    keymap("n", "gn", function()
       lsp_keyword_mark.goto_next_reference(false)
     end, default_opts)
-    keymap("n", "[", function()
+    keymap("n", "gp", function()
       lsp_keyword_mark.goto_prev_reference(false)
     end, default_opts)
   end,
