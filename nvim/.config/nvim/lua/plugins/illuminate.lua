457f27481881b5b6afb8d467d70f8213d37c5d1a
Dachi Chang <dachi.chang@idtechproducts.com>
2024-12-26 15:10:38 +0800

    bind more simple key to jump between lsp variable reference

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -6,10 +6,10 @@ return {
     local default_opts = { noremap = true, silent = true }
 
     local keymap = vim.keymap.set
-    keymap("n", "gn", function()
+    keymap("n", ")", function()
       lsp_keyword_mark.goto_next_reference(false)
     end, default_opts)
-    keymap("n", "gp", function()
+    keymap("n", "(", function()
       lsp_keyword_mark.goto_prev_reference(false)
     end, default_opts)
   end,
