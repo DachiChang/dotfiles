a5ad409a246e1253c807c969455c0be9b4bcb018
Dachi Chang <dachi.chang@idtechproducts.com>
2024-12-26 17:16:38 +0800

    more simple key binding

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -6,10 +6,10 @@ return {
     local default_opts = { noremap = true, silent = true }
 
     local keymap = vim.keymap.set
-    keymap("n", ")", function()
+    keymap("n", "J", function()
       lsp_keyword_mark.goto_next_reference(false)
     end, default_opts)
-    keymap("n", "(", function()
+    keymap("n", "K", function()
       lsp_keyword_mark.goto_prev_reference(false)
     end, default_opts)
   end,
