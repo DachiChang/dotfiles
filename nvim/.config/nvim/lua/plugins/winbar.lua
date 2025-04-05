166b5cfc19d5d292f197369264f143edac23947a
Dachi Chang <dachi.chang@idtechproducts.com>
2025-04-05 20:05:38 +0800

    refactor

--- nvim/.config/nvim/lua/plugins/winbar.lua
+++ nvim/.config/nvim/lua/plugins/winbar.lua
@@ -9,26 +9,28 @@ return {
       "TelescopeResults",
       "help",
       "lazy",
-      "markdown",
       "mason",
       "spectre_panel",
       "tagbar",
       "toggleterm",
     }
 
-    local api = vim.api
-    api.nvim_create_autocmd("FileType", {
+    vim.api.nvim_create_autocmd("BufWinEnter", {
       pattern = '*',
       callback = function()
-        if not vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
-          local file_absolute_dir = vim.fn.expand("%:p:h")
+        local win_config = vim.api.nvim_win_get_config(vim.api.nvim_get_current_win())
+        if not vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) and win_config.relative ~= "win" then
           local file_name = vim.fn.expand("%:t")
+          if file_name == "" then
+            return
+          end
+
+          local file_absolute_dir = vim.fn.expand("%:p:h")
           local relative_path = vim.fn.substitute(file_absolute_dir, vim.fn.getcwd(), "", "")
-          local icon = devicons.get_icon(file_absolute_dir .. "/" .. file_name)
-          if icon == nil then
-            icon = ""
-          else
-            icon = icon .. " "
+          local icon_str = ""
+          local icon = devicons.get_icon(file_name)
+          if icon ~= nil then
+            icon_str = icon .. " "
           end
           -- trim the start root slash and replace / to > and append icon and append filename.
           local file_path = ""
@@ -36,7 +38,7 @@ return {
             file_path = relative_path:gsub('^/', ''):gsub('/', ' ➤ ') .. " ➤ "
           end
 
-          vim.opt_local.winbar = " " .. file_path .. icon .. file_name .. " %m%#WinbarDelete#"
+          vim.opt_local.winbar = " " .. file_path .. icon_str .. file_name .. " %m%#WinbarDelete#"
         end
       end
     })
