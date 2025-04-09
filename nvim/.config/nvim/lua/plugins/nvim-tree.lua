64a8492f66ba84211422c22af1ce809e7d1e31fd
Dachi Chang <dachi.chang@idtechproducts.com>
2025-04-09 16:35:29 +0800

    featur sync cursor between tab

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -5,9 +5,8 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
+    local api = require('nvim-tree.api')
     local function on_attach(bufnr)
-      local api = require('nvim-tree.api')
-
       local function opts(desc)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
       end
@@ -88,6 +87,37 @@ return {
         dotfiles = true,
       },
     }
+
+    -- sync cursor between tab
+    local position = { 1, 0 }
+    local nvim_tree_event = api.events.Event
+    api.events.subscribe(nvim_tree_event.TreeRendered, function(event) -- 當 tree rendered 時重新對齊 cursor
+      vim.api.nvim_win_set_cursor(event.winnr, position)
+      -- vim.notify("Event set cursor" .. vim.inspect(position), vim.log.levels.WARN)
+    end)
+
+    local cursor_sync_group = vim.api.nvim_create_augroup("NvimTreeCursorSync", { clear = true })
+    vim.api.nvim_create_autocmd("CursorMoved", { -- 記錄 NvimTree 中 cursor 位置
+      group = cursor_sync_group,
+      pattern = "NvimTree_*",
+      callback = function()
+        position = vim.api.nvim_win_get_cursor(0)
+        -- vim.notify("Save cursor" .. vim.inspect(position), vim.log.levels.INFO)
+      end
+    })
+
+    vim.api.nvim_create_autocmd("TabEnter", {
+      group = cursor_sync_group,
+      pattern = "NvimTree_*",
+      callback = function()
+        -- NOTE: 如果都是在主視窗編輯切換 Tab 會讓 Event.TreeRendered 自設定 position 沒問題
+        -- 但是當 focus 在 NvimTree 的時候，切換 Tab 因為設定了 CursorMoved 因此會重新記錄 position
+        -- 這導致了在 focus NvimTree 相互切換時，Cursor 會一直先被重設為該 Buf 的 Cursor ，等於沒有同步
+        -- 優先叫用 tree.reload() 會觸發前面定義的 set cursor 這優先於 CursorMoved 所以可以正確同步
+        api.tree.reload()
+        -- vim.notify("TabEnter trigger tree reload" .. vim.inspect(position), vim.log.levels.ERROR)
+      end
+    })
 
     -- auto close
     vim.api.nvim_create_autocmd("BufEnter", {
