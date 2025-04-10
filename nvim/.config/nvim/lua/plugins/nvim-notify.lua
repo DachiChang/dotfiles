d44f9666fe4f113dc247ea05e635063e080566fa
Dachi Chang <dachi.chang@idtechproducts.com>
2025-04-10 10:36:37 +0800

    config

--- nvim/.config/nvim/lua/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/plugins/nvim-notify.lua
@@ -2,7 +2,10 @@ return {
   'rcarriga/nvim-notify',
   enabled = false, -- for debug use
   config = function()
-    require("notify").setup()
+    require("notify").setup {
+      background_colour = 'FloatShadow',
+      timeout = 3000,
+    }
     vim.notify = require('notify')
   end
 }
