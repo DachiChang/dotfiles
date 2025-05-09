d7c41571a319156e0b9ab075b69059fc3bdb3a11
Dachi Chang <dachi.chang@idtechproducts.com>
2025-05-09 17:03:55 +0800

    refactor for nvim v0.11

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,18 @@
 return {
-  'williamboman/mason.nvim', -- mason lsp and tools manager
+  'mason-org/mason.nvim', -- mason lsp and tools manager
   enabled = true,
   dependencies = {
-    'williamboman/mason-lspconfig.nvim',         -- bridge between client and mason
     'WhoIsSethDaniel/mason-tool-installer.nvim', -- auto install mason tools
-    'neovim/nvim-lspconfig',                     -- language server protocal client
-    'hrsh7th/cmp-nvim-lsp',                      -- bridge lspconfig
-    'hrsh7th/nvim-cmp',                          -- completion
+    'mason-org/mason-lspconfig.nvim',            -- bridge between mason package name and nvim-lspconfig required by mason-tool-installer
+    'neovim/nvim-lspconfig',                     -- language server default config
     'ray-x/lsp_signature.nvim',                  -- Show function signature when you type
-    'stevearc/conform.nvim',                     -- formatter
   },
   config = function()
     local keymap = vim.keymap.set
     local default_opts = { noremap = true, silent = true }
 
-    local lsp_server_configs = { -- ensure installed language server
+    -- Language Server configmap
+    local lsp_server_configs = {
       clangd = {
         filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
       },
@@ -45,17 +43,13 @@ return {
       templ = {},
     }
 
-    local mason_install_tools = {
+    -- tools configmap
+    local tools = {
       "prettier", -- prettier formatter
       "autopep8", -- python formatter
-      "buf",      -- proto formatter
     }
 
-    local mason_tool_installer = require('mason-tool-installer')
-    mason_tool_installer.setup {
-      ensure_installed = mason_install_tools,
-    }
-
+    -- Mason UI config
     local mason_installer = require('mason')
     mason_installer.setup {
       ui = {
@@ -68,84 +62,48 @@ return {
       }
     }
 
-    -- prepare lsp cmp injection
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    local buf = vim.lsp.buf
-    local on_attach = function(_, bufnr) -- NOTE: client, bufnr
-      local bufopts = { noremap = true, silent = true, buffer = bufnr }
-      keymap("n", "gt", function() buf.hover { border = 'rounded' } end, bufopts)
-      keymap("n", "gD", buf.declaration, bufopts)
-      keymap("n", "gd", ":Telescope lsp_definitions<CR>", bufopts)
-      keymap("n", "gi", ":Telescope lsp_implementations<CR>", bufopts)
-      keymap("n", "gT", ":Telescope lsp_type_definitions<CR>", bufopts)
-      keymap("n", "gr", ":Telescope lsp_references<CR>", bufopts)
-      keymap("n", "gn", buf.rename, bufopts)
-      keymap("n", "ga", buf.code_action, bufopts)
+    -- Mason-tool-installer install all
+    local ensured_installed_all = {}
+    for lsp_server, _ in pairs(lsp_server_configs) do -- parse key, table
+      table.insert(ensured_installed_all, lsp_server) -- collect server to ensured_installed_lsp_servers
     end
-
-    -- language server config
-    local ensured_installed_lsp_servers = {}
-    for lsp_server, _ in pairs(lsp_server_configs) do         -- parse key, table
-      table.insert(ensured_installed_lsp_servers, lsp_server) -- collect key to ensured_installed_lsp_servers
+    for _, tool in ipairs(tools) do
+      table.insert(ensured_installed_all, tool) -- collect tool to ensured_installed_lsp_servers
     end
-    local mason_lspconfig = require('mason-lspconfig')
-    mason_lspconfig.setup {
-      ensure_installed = ensured_installed_lsp_servers,
-      handlers = {
-        function(server_name)
-          local lsp_server_config = lsp_server_configs[server_name] or {} -- get lsp server configs
-          local lsp_configs = {
-            capabilities = capabilities,
-            on_attach = on_attach,
-          }
-          lsp_server_config = vim.tbl_deep_extend("force", lsp_configs, lsp_server_config) -- merge lsp server configs
-          require('lspconfig')[server_name].setup(lsp_server_config)
-        end,
-      },
+    local mason_tool_installer = require('mason-tool-installer')
+    mason_tool_installer.setup {
+      ensure_installed = ensured_installed_all,
     }
 
-    -- language formatter
-    local conform = require('conform')
-    conform.setup {
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        python = { "autopep8" },
-        templ = { "templ" }, -- NOTE: 由於 html, templ 都具有 formating 功能，因此由 conform 來指定，避免 fallback lsp formating 會有交錯 formating 的問題
-        proto = { "buf" },   -- NOTE: 由於 buf beta lsp 會過 lint 導致格式化失敗，因此由 conform 來指定，避免 lsp format 錯誤
-      },
-      -- format_on_save = {
-      --   timeout_ms = 3000,
-      --   lsp_format = "fallback",
-      -- },
-    }
-    keymap("n", "gf", function()
-      conform.format({
-        timeout_ms = 3000,
-        lsp_format = "fallback",
-      })
-    end, default_opts)
-
-    -- Show function signature when you type
-    local lsp_signature = require('lsp_signature')
-    lsp_signature.setup {
-      bind = true, -- This is mandatory, otherwise border config won't get registered.
-      hint_enable = false,
-      handler_opts = {
-        border = 'rounded',
-      }
-    }
-
-    -- nvim lsp config
+    -- global language server config
     local lsp = vim.lsp
     lsp.set_log_level('off') -- disable log or debug lsp
 
-    -- nvim lsp diagnostics config
+    -- global language server attach event
+    local lsp_attach_group = vim.api.nvim_create_augroup("NvimLspAttach", { clear = true })
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = lsp_attach_group,
+      callback = function(event)
+        local buf = vim.lsp.buf
+        local bufopts = { noremap = true, silent = true, buffer = event.buf }
+        keymap("n", "gt", function() buf.hover { border = 'rounded' } end, bufopts)
+        keymap("n", "gD", buf.declaration, bufopts)
+        keymap("n", "gd", ":Telescope lsp_definitions<CR>", bufopts)
+        keymap("n", "gi", ":Telescope lsp_implementations<CR>", bufopts)
+        keymap("n", "gT", ":Telescope lsp_type_definitions<CR>", bufopts)
+        keymap("n", "gr", ":Telescope lsp_references<CR>", bufopts)
+        keymap("n", "gn", buf.rename, bufopts)
+        keymap("n", "ga", buf.code_action, bufopts)
+      end,
+    })
+
+    -- For each language server config
+    for server, config in pairs(lsp_server_configs) do
+      vim.lsp.enable(server)
+      vim.lsp.config(server, config)
+    end
+
+    -- Diagnostics config
     local diagnostic = vim.diagnostic
     diagnostic.config {
       virtual_lines = {
@@ -161,5 +119,15 @@ return {
     keymap("n", "ge", diagnostic.open_float, default_opts)
     keymap("n", "ep", diagnostic.goto_prev, default_opts)
     keymap("n", "en", diagnostic.goto_next, default_opts)
+
+    -- Show function signature when you type
+    local lsp_signature = require('lsp_signature')
+    lsp_signature.setup {
+      bind = true, -- This is mandatory, otherwise border config won't get registered.
+      hint_enable = false,
+      handler_opts = {
+        border = 'rounded',
+      }
+    }
   end,
 }
