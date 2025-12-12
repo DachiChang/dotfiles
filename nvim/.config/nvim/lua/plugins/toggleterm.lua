114424bc27d4a7afb869d56005c8da104cac3e33
Dachi Chang <dachi.chang@idtechproducts.com>
2025-12-12 09:44:22 +0800

    use leader + 1 ~ 9 to switch terminals

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     local toggleterm = require('toggleterm')
     toggleterm.setup({
-      open_mapping = [[<c-\>]],
       direction = "float",
       float_opts = {
         border = 'curved',
@@ -14,6 +13,10 @@ return {
     local keymap = vim.keymap.set
     local default_opts = { noremap = true, silent = true }
     -- ESC to exist terminal mode to normal mode
-    keymap("t", "<LEADER><ESC>", [[<C-\><C-n>]], default_opts)
+    keymap("t", "<LEADER><ESC>", "<C-\\><C-n>", default_opts)
+    -- Prepare 1 ~ 9 terminal, 0 can't be a terminal count
+    for i = 1, 9 do
+      keymap({"n", "t"}, "<LEADER>" .. i, "<CMD>ToggleTerm" .. i .. "<CR>")
+    end
   end,
 }
