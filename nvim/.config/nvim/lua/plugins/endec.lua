75f9fe8ce995fdabf36265dade195567b75be809
Dachi Chang <dachi.chang@idtechproducts.com>
2025-12-14 16:12:01 +0800

    refactor config

--- nvim/.config/nvim/lua/plugins/endec.lua
+++ nvim/.config/nvim/lua/plugins/endec.lua
@@ -5,20 +5,12 @@ return {
   config = function()
     require("endec").setup({
       keymaps = {
+        -- disable all default keymap
+        defaults = false,
         -- b64 b64URL URL encode/decode
         decode_base64_popup = "gbs",   -- base64 show
         decode_base64_inplace = "gbd", -- base64 decode
         encode_base64_inplace = "gbe", -- base64 encode
-
-        -- disable base64 url encode/decode
-        decode_base64url_popup = "",   -- base64 url show
-        decode_base64url_inplace = "", -- base64 url decode
-        encode_base64url_inplace = "", -- base64 url encode
-
-        -- disable url encode/decode
-        decode_url_popup = "",   -- url show
-        decode_url_inplace = "", -- url decode
-        encode_url_inplace = "", -- url encode
       },
     })
   end,
