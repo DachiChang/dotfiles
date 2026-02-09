833d6e158b444f157574c4bb09acd72d46992aef
Dachi Chang <dachi.chang@idtechproducts.com>
2026-02-09 10:52:20 +0800

    disable name color

--- nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -5,6 +5,7 @@ return {
     local colorizer = require('colorizer')
     colorizer.setup({
       user_default_options = {
+        names = false, -- disable Name color ex: Red, Blue
         tailwind = true,
       }
     })
