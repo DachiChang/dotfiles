36264fd417773753a6a4acf2c0e333153248d061
Dachi Chang <dachi.chang@idtechproducts.com>
2026-05-11 17:37:04 +0800

    use treesitter-manager to be treesitter parsers installer.

---
+++ nvim/.config/nvim/lua/plugins/treesitter-manager.lua
@@ -0,0 +1,30 @@
+-- REQUIRED: brew install tree-sitter-cli
+return {
+  'romus204/tree-sitter-manager.nvim',
+  enabled = true,
+  config = function()
+    local tree_sitter_manager = require('tree-sitter-manager')
+    tree_sitter_manager.setup({
+      auto_install = true,
+      highlight = false, -- Self active for runtime highlight after parser nstalled.
+    })
+
+    -- Active treesitter
+    local treesitter_setup_augroup = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = '*',
+      group = treesitter_setup_augroup,
+      callback = function(args)
+        local filetype = args.match
+        local buf = args.buf
+        -- Get filetype registered language
+        local language = vim.treesitter.language.get_lang(filetype)
+
+        -- Try to load treesitter parser
+        if vim.treesitter.language.add(language) then
+          vim.treesitter.start(buf, language)
+        end
+      end,
+    })
+  end
+}
