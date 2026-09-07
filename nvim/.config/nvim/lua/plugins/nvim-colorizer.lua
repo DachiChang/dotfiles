60da12265d2a1a46078aacb55b3b122bd123aaef
Dachi Chang <dachi.chang@idtechproducts.com>
2026-09-07 09:31:04 +0800

    fix disable for togglerterm

--- nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     local colorizer = require('colorizer')
     colorizer.setup({
+      filetypes = { "*", "!toggleterm" },
       user_default_options = {
-        buftypes = { "" }, -- only support normal buftypes
-        names = false,     -- disable Name color ex: Red, Blue
+        names = false, -- disable Name color ex: Red, Blue
         tailwind = true,
       }
     })
