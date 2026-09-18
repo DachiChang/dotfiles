f005f891f7d31334d561040f8bd9e59132f376ad
Dachi Chang <dachi.chang@idtechproducts.com>
2026-09-18 11:07:24 +0800

    add tf formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -18,6 +18,7 @@ return {
         python = { "autopep8" }, -- WARN: Mason install
         templ = { "templ" },     -- NOTE: 由於 html, templ 都具有 formating 功能，因此由 conform 來指定，避免 fallback lsp formating 會有交錯 formating 的問題
         proto = { "buf" },       -- NOTE: 由於 buf beta lsp 會過 lint 導致格式化失敗，因此由 conform 來指定，避免 lsp format 錯誤
+        terraform = { "terraform_fmt" },
       },
       -- format_on_save = {
       --   timeout_ms = 3000,
