0cc69e00df20dce842ab4e683dcfdb43d288f31f
Dachi Chang <dachi.chang@idtechproducts.com>
2026-09-23 15:43:06 +0800

    for best practice

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -7,6 +7,10 @@ return {
       auto_restore = false,
     })
 
+    -- `vim.o.sessionoptions` should contain 'localoptions' to make sure
+    -- filetype and highlighting work correctly after a session is restored.
+    vim.opt.sessionoptions:append("localoptions")
+
     local keymap = vim.keymap.set
 
     keymap("n", "<LEADER><BS>", ":AutoSession restore<CR>") -- back
