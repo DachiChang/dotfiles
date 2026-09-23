97c4977d9aff3302f3aa3406b0b601414ff88c9d
Dachi Chang <dachi.chang@idtechproducts.com>
2026-09-23 15:46:27 +0800

    need to init first

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,15 +1,16 @@
 return {
   "rmagatti/auto-session",
   enabled = true,
+  init = function()
+    -- `vim.o.sessionoptions` should contain 'localoptions' to make sure
+    -- filetype and highlighting work correctly after a session is restored.
+    vim.opt.sessionoptions:append("localoptions")
+  end,
   config = function()
     local auto_session = require("auto-session")
     auto_session.setup({
       auto_restore = false,
     })
-
-    -- `vim.o.sessionoptions` should contain 'localoptions' to make sure
-    -- filetype and highlighting work correctly after a session is restored.
-    vim.opt.sessionoptions:append("localoptions")
 
     local keymap = vim.keymap.set
 
